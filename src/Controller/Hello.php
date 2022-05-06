435a2a271bc21dda4d1ff56c5742ecc367f982e0
Danila <qlyouqwa4ka@gmail.com>
2022-05-06 14:26:30 +0300

    fix logic of Controllers

--- src/Controller/Hello.php
+++ src/Controller/Hello.php
@@ -1,19 +1,9 @@
 <?php
-namespace App\Controller;
+    
+    namespace App\Controller;
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
-
-class Hello {
-    public function display() {
-        $app = AppFactory::create();
-
-        $app->get('/hello', function (Request $request, Response $response) {
-            $response->getBody()->write('Hello World');
-            return $response;
-        });
-
-        $app->run();
+    class Hello {
+        public function display() {
+            return 'HelloWorld!';
+        }
     }
-}
